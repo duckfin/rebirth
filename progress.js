620ff565d8010496b42ad0aa3689539f83155b24
Austin Morgan <codenameduckfin@gmail.com>
2016-11-11 16:48:57 -0500

    Create progress.js

---
+++ progress.js
@@ -0,0 +1,11 @@
+var progress = {};
+
+progress.curDay = {}
+progress.generateDay = function (d,y) {
+  this.curDay[0] = [character.rest,1];
+  this.curDay[1] = [character.rest,1];
+  this.curDay[2] = [character.rest,1];
+  this.curDay[3] = [character.rest,1];
+  this.curDay[4] = [character.rest,1];
+  this.curDay[5] = [character.rest,1];
+}
