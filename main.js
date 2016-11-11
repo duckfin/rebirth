b53666222eae7b3b14db69ad70679f36742fae4c
Austin Morgan <codenameduckfin@gmail.com>
2016-11-11 15:31:25 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var character = {};
 character.stats = {"STR":10,"DEX":10,"CON":10,"INT":10,"WIS":10,"CHA":10};
-charcter.updateStats = function () {
+character.updateStats = function () {
   for(var s of this.stats){
     document.getElementById(s).innerHTML=this.stats[s];
   }
