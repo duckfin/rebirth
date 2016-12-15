781010d0f9977c23d6993097de8ca3550cf81d09
Austin Morgan <codenameduckfin@gmail.com>
2016-12-15 12:56:20 -0500

    Update progress.js

--- progress.js
+++ progress.js
@@ -1,11 +1,3 @@
-var progress = {};
+var game = {}
 
-progress.curDay = {}
-progress.generateDay = function (d,y) {
-  this.curDay[0] = [character.rest,1];
-  this.curDay[1] = [character.rest,1];
-  this.curDay[2] = [character.rest,1];
-  this.curDay[3] = [character.rest,1];
-  this.curDay[4] = [character.rest,1];
-  this.curDay[5] = [character.rest,1];
-}
+
