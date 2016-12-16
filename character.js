54538c4059002951ac38c332b517ee49fdb8ca2e
Austin Morgan <codenameduckfin@gmail.com>
2016-12-16 13:19:05 -0500

    Update character.js

--- character.js
+++ character.js
@@ -6,8 +6,11 @@ character.resting = 0;
 
 character.baseDamageMin = 0;
 character.baseDamageMax = 2;
+character.baseHealMin = 1;
+character.baseHealMax = 2;
 
 character.attackModifier = function () {return (1+0.01*character.stats["STR"]);}
+character.healModifier = function () {return (1+0.01*character.stats["WIS"]);}
 
 character.rollAttack = function () {return character.attackModifier()*(Math.random()*(character.baseDamageMax-character.baseDamageMin)+character.baseDamageMin);}
 
@@ -28,8 +31,22 @@ character.changeMaxStat = function (s,a){
     console.log("Error changing stat "+s);
   }
 }
+
+character.takeDamage = function (a) {
+  character.stats["curHP"] -= a;
+  if(character.stats["curHP"]<=0){
+    character.stats["curHP"]=0;
+    character.resting=1;
+  }
+}
+
 character.rest = function () {
-  
+  var h = character.healModifier()*(Math.random()*(character.baseHealMax-character.baseHealMin)+character.baseHealMin);
+  character.stats["curHP"]+=h;
+  if(character.stats["curHP"]>=character.stats["maxHP"]){
+    character.resting = 0;
+    character.stats["curHP"] = character.stats["maxHP"];
+  }
 }
 
 character.updateStats = function () {
