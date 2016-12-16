d3ad95aff591086f1d39b14eaf090726741809da
Austin Morgan <codenameduckfin@gmail.com>
2016-12-16 10:44:16 -0500

    Update progress.js

--- progress.js
+++ progress.js
@@ -6,10 +6,14 @@ game.currentQuest.encounters = [];
 
 function generateQuest(qlev){
   for(var i=0;i<100<i++){
-    game.currentQuest.encounters[i] = generateEncounter(qlev,i);
+    game.currentQuest.encounters[i] = new Encounter(qlev,i);
   }
 }
 
-function generateEncounter(qlev,eind) {
-  
+function Encounter(qlev,eind) {
+  this.qlev = qlev;
+  this.eind = eind;
+  this.name = "Thing";
+  this.damage = 1;
+  this.health = 1;
 }
