1b856063084526c6d38f172f4da70bd51e75be70
Austin Morgan <codenameduckfin@gmail.com>
2016-12-16 12:44:34 -0500

    Update character.js

--- character.js
+++ character.js
@@ -1,8 +1,14 @@
 var character = {};
 
-character.stats = {"STR":10,"DEX":10,"CON":10,"INT":10,"WIS":10,"CHA":10};
-character.maxStats = {"STR":10,"DEX":10,"CON":10,"INT":10,"WIS":10,"CHA":10};
+character.stats = {"STR":10,"DEX":10,"CON":10,"INT":10,"WIS":10,"CHA":10,"curHP":10,"maxHP":10};
 character.year = 0;
+
+character.baseDamageMin = 0;
+character.baseDamageMax = 2;
+
+character.attackModifier = function () {return (1+0.01*character.stats["STR"]);}
+
+character.rollAttack = function () {return character.attackModifier()*(Math.random()*(character.baseDamageMax-character.baseDamageMin)+character.baseDamageMin);}
 
 character.changeCurStat = function (s,a){
   try{
