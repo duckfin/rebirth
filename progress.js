35cc3ae039e015e820217b8074d971ff065f9f5f
Austin Morgan <codenameduckfin@gmail.com>
2016-12-16 09:39:58 -0500

    Update progress.js

--- progress.js
+++ progress.js
@@ -1,3 +1,15 @@
-var game = {}
+var game = {};
+game.currentQuest = {};
+game.currentQuest.encounters = [];
 
 
+
+function generateQuest(qlev){
+  for(var i=0;i<100<i++){
+    game.currentQuest.encounters[i] = generateEncounter(qlev,i);
+  }
+}
+
+function generateEncounter(qlev,eind) {
+  
+}
