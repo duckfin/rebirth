6ced4b809ac603ad4b3efa0a9c1bbbbe4fd540ca
Austin Morgan <codenameduckfin@gmail.com>
2016-12-25 17:46:59 -0500

    Update progress.js

--- progress.js
+++ progress.js
@@ -34,6 +34,7 @@ game.mainLoop = function () {
 
 game.currentQuest.advance = function () {
   character.stats["Gold"] += game.currentQuest.encounters[game.currentQuest.currentEncInd].qlev;
+  character.gainXP(1);
   game.updateQuestInfo();
   if(game.currentQuest.encounters[game.currentQuest.currentEncInd+1]){game.currentQuest.currentEncInd+=1;}
   else {game.currentQuest.currentEncInd=0;generateQuest(game.currentQuest.qlev+1);}
