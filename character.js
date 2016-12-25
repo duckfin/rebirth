54e947ed75ea456d6309aaf9db1e209289eb529d
Austin Morgan <codenameduckfin@gmail.com>
2016-12-25 17:46:24 -0500

    Update character.js

--- character.js
+++ character.js
@@ -1,9 +1,10 @@
 var character = {};
 
-character.stats = {"STR":10,"DEX":10,"CON":10,"INT":10,"WIS":10,"CHA":10,"curHP":10,"maxHP":10,"Gold":100};
+character.stats = {"STR":10,"DEX":10,"CON":10,"INT":10,"WIS":10,"CHA":10,"curHP":10,"maxHP":10,"Gold":100,"XP":0};
 character.statCost = {"STR":10,"DEX":10,"CON":10,"INT":10,"WIS":10,"CHA":10};
 character.year = 0;
 character.resting = 0;
+character.nextLevelXP = 100;
 
 character.baseDamageMin = 0;
 character.baseDamageMax = 2;
