3d0a4c5f29b85a6e4884ea1bb883bfa523ca716b
Austin Morgan <codenameduckfin@gmail.com>
2017-01-23 12:23:14 -0500

    Update progress.js

--- progress.js
+++ progress.js
@@ -28,6 +28,7 @@ game.mainLoop = function () {
     }
   }
   else {character.rest();var out = "Character rested.";}
+  if(character.trainingStat){character.gainStatXP(character.trainingStat,1);}
   character.updateStats();
   console.log(out);
 }
