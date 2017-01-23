0d3ea55e7f8e56063ed6134d33bb613fcf647c5c
Austin Morgan <codenameduckfin@gmail.com>
2017-01-23 12:58:51 -0500

    Update boons.js

--- boons.js
+++ boons.js
@@ -18,6 +18,11 @@ var Boon = function(name,type,a,b,c,d,e,f,g,func){
     }
     this.func();
   }
+  var i = document.createElement("input");
+  i.type = "button";
+  i.value = name;
+  i.onclick = this.onTake;
+  document.getElementById(type+"Boons").appendChild(i);
   console.log("Boon "+name+" created and added to list.");
 }
 
